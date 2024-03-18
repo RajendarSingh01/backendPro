8265f14e884c0ffb7acae49c55ac899ac28445c5
Rajendar <raa.singh0000@gmail.com>
2024-03-18 17:16:24 +0530

    database Connected

---
+++ src/db/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+    console.log("Database is conneted");
+  } catch (error) {
+    console.log("MongoDB Connection Error ", error);
+    process.exit(1);
+  }
+};
+export default connectDB;
