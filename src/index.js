b7422ac8f2500bce7406c9c7b7f61b1949048f05
Rajendar <raa.singh0000@gmail.com>
2024-03-20 07:10:42 +0530

    created utils api errors api respone and asynchHandle

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import dotenv from "dotenv";
 import connectDB from "./db/db.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
+const port = process.env.PORT || 4000;
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`app is listing on prot ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`fail to connect to database err`, err);
+  });
